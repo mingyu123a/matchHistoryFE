71997140aa4d398984c78f4eb10ad7268672492c
mingyu123a <94092165+mingyu123a@users.noreply.github.com>
2024-09-08 15:43:38 +0900

    Update riotAPI.js

--- public/riotAPI.js
+++ public/riotAPI.js
@@ -1,6 +1,6 @@
 function getRiotApiKey() {
     $.ajax({
-        url: "localhost:8080/api/config", // API URL
+        url: "http://localhost:8080/api/config", // API URL
         method: 'GET', // 요청 방식
         success: function(response) {
             riotApiKey = JSON.stringify(response)
