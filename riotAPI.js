c09af3f79aa4b26393c9bb134e7b1f4f78f01b02
mingyu123a <mingyuhan123a@gmail.com>
2024-09-08 15:13:45 +0900

    riotAPIKey

--- riotAPI.js
+++ riotAPI.js
@@ -4,6 +4,7 @@ function getRiotApiKey() {
         method: 'GET', // 요청 방식
         success: function(response) {
             riotApiKey = JSON.stringify(response)
+            console.log(riotApiKey)
         },
         error: function(error) {
             console.log('API키를 불러오는데 실패했습니다.', error);
