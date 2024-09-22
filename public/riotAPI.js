f36bbce4f85c8bec63a5594872f0442e8176e743
mingyu123a <94092165+mingyu123a@users.noreply.github.com>
2024-09-22 13:16:16 +0900

    Update riotAPI.js

--- public/riotAPI.js
+++ public/riotAPI.js
@@ -5,6 +5,7 @@ function getRiotApiKey() {
         success: function(response) {
             riotApiKey = JSON.stringify(response)
             console.log(riotApiKey)
+            riotApiKey = riotApiKey.replace(/^"|"$/g, '');
         },
         error: function(error) {
             console.log('API키를 불러오는데 실패했습니다.', error);
