613be347a1f476a8ad72c99c771cb8885c71b64c
mingyu123a <mingyuhan123a@gmail.com>
2024-09-22 13:34:44 +0900

    8080 -> ec2

--- public/riotAPI.js
+++ public/riotAPI.js
@@ -1,18 +1,15 @@
-function getRiotApiKey() {
-    var riotApiKey;
-    $.ajax({
-        url: "https://findd.findduo.site/api/config", // API URL
-        method: 'GET', // 요청 방식
-        success: function(response) {
-            riotApiKey = JSON.stringify(response)
-            riotApiKey = riotApiKey.replace(/^"|"$/g, '');
-            console.log(riotApiKey);
-        },
-        error: function(error) {
-            console.log('API키를 불러오는데 실패했습니다.', error);
-            return "default";
-        }
-    });
-    return riotApiKey;
+async function getRiotApiKey() {
+    try {
+        const response = await $.ajax({
+            url: "https://findd.findduo.site/api/config",
+            method: 'GET',
+        });
+        let riotApiKey = JSON.stringify(response);
+        riotApiKey = riotApiKey.replace(/^"|"$/g, '');
+        console.log(riotApiKey);
+        return riotApiKey;
+    } catch (error) {
+        console.log('API키를 불러오는데 실패했습니다.', error);
+        return "default";
+    }
 }
-
